2c2df845fc79961d9eda8aa523de1c8716cb10d4
zulfaz <zahrozulfa516@gmail.com>
2025-12-03 14:05:55 +0700

    add logout button

--- src/app/results/page.js
+++ src/app/results/page.js
@@ -1,4 +1,5 @@
 "use client";
+import { signOut } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { track, artist, playlist } from "@/assets";
 import ResultTitle from "@/components/ResultTitle";
@@ -21,22 +22,22 @@ const ResultsPage = () => {
       fetch("/api/top-albums").then((res) => res.json()),
       fetch("/api/minutes").then((res) => res.json()),
     ])
-    .then(([userName, tracksData, artistsData, albumsData, minutesData]) => {
-      setUser(userName.name || "");
-      setTracks(tracksData.items || []);
-      setArtists(artistsData.items || []);
-      setAlbums(albumsData.items || []);
-      setMinutes(minutesData.totalMinutes || 0);
-    })
-    .catch((err) => console.error("Error fetching Wrapped data:", err))
+      .then(([userName, tracksData, artistsData, albumsData, minutesData]) => {
+        setUser(userName.name || "");
+        setTracks(tracksData.items || []);
+        setArtists(artistsData.items || []);
+        setAlbums(albumsData.items || []);
+        setMinutes(minutesData.totalMinutes || 0);
+      })
+      .catch((err) => console.error("Error fetching Wrapped data:", err));
   }, []);
 
   return (
     <main className="bg-galaxy-results min-h-screen flex flex-col justify-center items-center py-16 gap-10 w-full">
       {/* head */}
-      <ResultTitle user={user}/>
+      <ResultTitle user={user} />
 
-{/* top list */}
+      {/* top list */}
       <div className="flex flex-col gap-9 justify-center md:flex-row md:justify-between items-center w-full max-w-2xl md:max-w-5xl">
         <TopList
           data={tracks}
@@ -57,9 +58,9 @@ const ResultsPage = () => {
         />
       </div>
 
-      <Minutes minutes={minutes}/>
+      <Minutes minutes={minutes} />
       <ButtonAuth
-        // onClick={w}
+        onClick={() => signOut({callbackUrl: "/"})}
         label="Log out"
       />
     </main>
