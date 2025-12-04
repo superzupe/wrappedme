b21c4d5a40afa20b0d62319e636b249cfe48abf8
zulfaz <zahrozulfa516@gmail.com>
2025-12-04 13:33:40 +0700

    sayang sea dechchart

--- src/components/PrimaryButton.js
+++ src/components/PrimaryButton.js
@@ -3,7 +3,7 @@
 const PrimaryButton = ({ onClick, label }) => {
   return (
     <button
-    type="submit"
+    type="button"
       onClick={onClick}
       className="flex flex-row justify-center w-full px-10 py-3 bg-btn-spotify text-text-active font-black text-md rounded-full transition-all duration-300 ease-in-out cursor-pointer hover:ring-4 hover:ring-btn-spotify"
     >
